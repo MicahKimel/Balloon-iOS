7009e28c723ec4c10ee839ef699bf6889cd693ac
mkimelCobec <mkimel@cobec.com>
2026-02-13 11:12:33 -0500

    Initial Commit

---
+++ BalloonDodge/BalloonDodgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalloonDodgeApp.swift
+//  BalloonDodge
+//
+//  Created by Micah Kimel on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BalloonDodgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BalloonDodgeTests/BalloonDodgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BalloonDodgeTests.swift
+//  BalloonDodgeTests
+//
+//  Created by Micah Kimel on 2/13/26.
+//
+
+import Testing
+@testable import BalloonDodge
+
+struct BalloonDodgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
